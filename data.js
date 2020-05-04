df30a509d12a7cd8b1ea222ee2a98cceb451b803
Hendrik Van Dijck <hendrikvandijck@MacBook-Pro.local>
2020-05-04 22:36:26 +0200

    basic files

---
+++ data.js
@@ -0,0 +1,1 @@
+var dataSubscriptions = { 'accounts': [ 'Werner', 'Isolde' ,'Gregory'], 'topics': [ 'Soccer', 'Basketball', 'Skating', 'Tennis', 'Running' ], 'accountSubscribedTopic': [ { 'topic':'Soccer', 'Account':'Simpson' }, { 'topic':'Soccer', 'Account':'Simpson' } ] }
