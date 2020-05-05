5ed4f4128315b35484f3e204f5f07917753336c3
Hendrik Van Dijck <hendrikvandijck@MacBook-Pro.local>
2020-05-05 17:01:16 +0200

    code refactor including previous code which was not commited

--- data.js
+++ data.js
@@ -5,14 +5,53 @@ var topics = ['Soccer', 'Basketball', 'Skating', 'Tennis', 'Running']
 var accountTopicSubscriptions = []
 
 showList = (type, listElementId) => {
-    var list = document.getElementById(listElementId)
+    // var list = document.getElementById(listElementId)
 
     type.forEach(element => {
-        let listItem = document.createElement('li')
-        listItem.appendChild(document.createTextNode(element))
-        list.appendChild(listItem)
+        createListItem(listElementId, element)
+
+        // let listItem = document.createElement('li')
+        // var documentFragment = document.createDocumentFragment();
+        // documentFragment.appendChild(listItem);
+
+        // let listItemButton = document.createElement('button')
+        // listItemButton.innerText = "subscribe"
+        // console.log(listItemButton)
+        // listItem.appendChild(document.createTextNode(element))
+        // listItem.appendChild(listItemButton)
+        // list.appendChild(listItem)
     });
 }
 
-showList(accounts,'accountList')
-showList(topics,'topicList')
+addSubscription = () => {
+    accountTopicSubscriptions.push(["Werner", "Soccer"])
+    let accountTopicSubscriptionsList = document.getElementById('accountTopicSubscriptionsList')
+
+
+
+    let accountTopicSubscriptionsListItem = document.createElement('li')
+    accountTopicSubscriptionsListItem.appendChild(document.createTextNode('zwavel', 'zuur'))
+    accountTopicSubscriptionsList.appendChild(accountTopicSubscriptionsListItem)
+
+    console.log(accountTopicSubscriptions)
+}
+
+createListItem = (listElementId, data) => {
+
+    let list = document.getElementById(listElementId)
+
+    let listItem = document.createElement('li')
+    let documentFragment = document.createDocumentFragment();
+        documentFragment.appendChild(listItem);
+
+        let listItemButton = document.createElement('button')
+        listItemButton.innerText = "subscribe"
+        console.log(listItemButton)
+        listItem.appendChild(document.createTextNode(data))
+        listItem.appendChild(listItemButton)
+        list.appendChild(listItem)
+}
+
+showList(accounts, 'accountList')
+showList(topics, 'topicList')
+showList(accountTopicSubscriptions, 'accountTopicSubscriptionsList')
