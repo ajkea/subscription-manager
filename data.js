ad015fa3519b8e3f3b8972635e471f7ce8b5a485
Hendrik Van Dijck <hendrikvandijck@MacBook-Pro.local>
2020-05-05 14:25:42 +0200

    updated version using arrays

--- data.js
+++ data.js
@@ -1,1 +1,15 @@
-var dataSubscriptions = { 'accounts': [ 'Werner', 'Isolde' ,'Gregory'], 'topics': [ 'Soccer', 'Basketball', 'Skating', 'Tennis', 'Running' ], 'accountSubscribedTopic': [ { 'topic':'Soccer', 'Account':'Simpson' }, { 'topic':'Soccer', 'Account':'Simpson' } ] }
+// var dataSubscriptions = { 'accounts': ['Werner', 'Isolde', 'Gregory'], 'topics': ['Soccer', 'Basketball', 'Skating', 'Tennis', 'Running'], 'accountSubscribedTopic': [{ 'topic': 'Soccer', 'Account': 'Simpson' }, { 'topic': 'Soccer', 'Account': 'Simpson' }] }
+
+var accounts = ['Werner', 'Isolde', 'Gregory']
+var topics = ['Soccer', 'Basketball', 'Skating', 'Tennis', 'Running']
+var accountTopicSubscriptions = []
+
+
+var showAccountList = () => {
+    document.getElementById('accountList').innerHTML = ` 
+<ul>
+${accounts.map(element => {
+        return (`<li> ${element} </li>`)
+    })}
+</ul>`
+}
