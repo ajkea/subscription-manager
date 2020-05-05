cda72f78a00a3c60380dfb908f40ae156608d11c
Hendrik Van Dijck <hendrikvandijck@MacBook-Pro.local>
2020-05-05 18:28:15 +0200

    add subscription functionality

--- data.js
+++ data.js
@@ -1,55 +1,46 @@
-// var dataSubscriptions = { 'accounts': ['Werner', 'Isolde', 'Gregory'], 'topics': ['Soccer', 'Basketball', 'Skating', 'Tennis', 'Running'], 'accountSubscribedTopic': [{ 'topic': 'Soccer', 'Account': 'Simpson' }, { 'topic': 'Soccer', 'Account': 'Simpson' }] }
-
 var accounts = ['Werner', 'Isolde', 'Gregory']
 var topics = ['Soccer', 'Basketball', 'Skating', 'Tennis', 'Running']
 var accountTopicSubscriptions = []
+var email
 
 showList = (type, listElementId) => {
-    // var list = document.getElementById(listElementId)
 
     type.forEach(element => {
         createListItem(listElementId, element)
-
-        // let listItem = document.createElement('li')
-        // var documentFragment = document.createDocumentFragment();
-        // documentFragment.appendChild(listItem);
-
-        // let listItemButton = document.createElement('button')
-        // listItemButton.innerText = "subscribe"
-        // console.log(listItemButton)
-        // listItem.appendChild(document.createTextNode(element))
-        // listItem.appendChild(listItemButton)
-        // list.appendChild(listItem)
     });
 }
 
-addSubscription = () => {
-    accountTopicSubscriptions.push(["Werner", "Soccer"])
-    let accountTopicSubscriptionsList = document.getElementById('accountTopicSubscriptionsList')
-
-
-
-    let accountTopicSubscriptionsListItem = document.createElement('li')
-    accountTopicSubscriptionsListItem.appendChild(document.createTextNode('zwavel', 'zuur'))
-    accountTopicSubscriptionsList.appendChild(accountTopicSubscriptionsListItem)
-
-    console.log(accountTopicSubscriptions)
+var updateEmail = (inputTextfield) => {
+    email = inputTextfield
+    let buttons = document.getElementsByTagName('button')
+    for(let button of buttons){
+        button.style.pointerEvents = 'visible'
+    }
 }
 
-createListItem = (listElementId, data) => {
+var addSubscription = (topic) => {
 
+    accountTopicSubscriptions.push([email, topic])
+    let accountTopicSubscriptionsList = document.getElementById('accountTopicSubscriptionsList')
+
+    let accountTopicSubscriptionsListItem = document.createElement('li')
+    accountTopicSubscriptionsListItem.appendChild(document.createTextNode([email, topic]))
+    accountTopicSubscriptionsList.appendChild(accountTopicSubscriptionsListItem)
+}
+
+var createListItem = (listElementId, data) => {
     let list = document.getElementById(listElementId)
 
     let listItem = document.createElement('li')
     let documentFragment = document.createDocumentFragment();
-        documentFragment.appendChild(listItem);
+    documentFragment.appendChild(listItem);
 
-        let listItemButton = document.createElement('button')
-        listItemButton.innerText = "subscribe"
-        console.log(listItemButton)
-        listItem.appendChild(document.createTextNode(data))
-        listItem.appendChild(listItemButton)
-        list.appendChild(listItem)
+    let listItemButton = document.createElement('button')
+    listItemButton.innerText = "subscribe"
+    listItemButton.onclick = () => addSubscription(data)
+    listItem.appendChild(document.createTextNode(data))
+    listItem.appendChild(listItemButton)
+    list.appendChild(listItem)
 }
 
 showList(accounts, 'accountList')
