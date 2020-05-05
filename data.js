2c5c52f151196ddea99807febe6976396bbedd9a
Hendrik Van Dijck <hendrikvandijck@MacBook-Pro.local>
2020-05-05 20:11:21 +0200

    add user and topics, check for doubles, stop showing buttons behind accounts

--- data.js
+++ data.js
@@ -3,46 +3,57 @@ var topics = ['Soccer', 'Basketball', 'Skating', 'Tennis', 'Running']
 var accountTopicSubscriptions = []
 var email
 
-showList = (type, listElementId) => {
+showList = (type, listElementId, category) => {
 
     type.forEach(element => {
-        createListItem(listElementId, element)
+        createListItem(listElementId, element, category)
     });
 }
 
 var updateEmail = (inputTextfield) => {
     email = inputTextfield
     let buttons = document.getElementsByTagName('button')
-    for(let button of buttons){
+    for (let button of buttons) {
         button.style.pointerEvents = 'visible'
+        button.style.backgroundColor = 'white'
     }
 }
 
 var addSubscription = (topic) => {
+    let stringDifferenceCheck;
 
-    accountTopicSubscriptions.push([email, topic])
-    let accountTopicSubscriptionsList = document.getElementById('accountTopicSubscriptionsList')
+    let double = false
+    for (let i = 0; i < accountTopicSubscriptions.length; i++) {
+        stringDifferenceCheck = accountTopicSubscriptions[0, i].includes('test')
+    }
 
-    let accountTopicSubscriptionsListItem = document.createElement('li')
-    accountTopicSubscriptionsListItem.appendChild(document.createTextNode([email, topic]))
-    accountTopicSubscriptionsList.appendChild(accountTopicSubscriptionsListItem)
+    if (!stringDifferenceCheck) {
+        accountTopicSubscriptions.push([email, topic])
+        let accountTopicSubscriptionsList = document.getElementById('accountTopicSubscriptionsList')
+
+        let accountTopicSubscriptionsListItem = document.createElement('li')
+        accountTopicSubscriptionsListItem.appendChild(document.createTextNode([email, topic]))
+        accountTopicSubscriptionsList.appendChild(accountTopicSubscriptionsListItem)
+    }
 }
 
-var createListItem = (listElementId, data) => {
+var createListItem = (listElementId, data, type) => {
     let list = document.getElementById(listElementId)
 
     let listItem = document.createElement('li')
     let documentFragment = document.createDocumentFragment();
     documentFragment.appendChild(listItem);
 
+    listItem.appendChild(document.createTextNode(data))
+    list.appendChild(listItem)
+    if(type!=='account'){
     let listItemButton = document.createElement('button')
     listItemButton.innerText = "subscribe"
     listItemButton.onclick = () => addSubscription(data)
-    listItem.appendChild(document.createTextNode(data))
     listItem.appendChild(listItemButton)
-    list.appendChild(listItem)
+    }
 }
 
-showList(accounts, 'accountList')
+showList(accounts, 'accountList', 'account')
 showList(topics, 'topicList')
 showList(accountTopicSubscriptions, 'accountTopicSubscriptionsList')
